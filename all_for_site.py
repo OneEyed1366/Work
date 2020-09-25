d25db4624d3b45961af64334257b1f1acbdfb703
Andrey <hidden_assassin@mail.ru>
2020-09-25 22:02:56 +0300

    Переписал скрипт для "правильного" копирования

--- all_for_site.py
+++ all_for_site.py
@@ -15,7 +15,7 @@ def search(path):
         return str(path).replace('_', '?')
     elif os.path.exists(str(path).replace('?', '_')):
         return str(path).replace('?', '_')
-
+    
 if os.sys.platform == 'ios':
     os.chdir(
         os.path.join(
@@ -49,10 +49,11 @@ home_dir = os.path.join(
     'familyan'
     )
 list_of_dirs = [
-    os.path.join(home_dir, 'static', 'images', 'Объекты'),
-    os.path.join(home_dir, 'static', 'images', 'Команда'),
-    os.path.join(home_dir, 'static', 'bd'),
-    os.path.join(home_dir, 'static', 'texts'),
+    os.path.join(home_dir, 'media', 'raw', 'images', 'properties'),
+    os.path.join(home_dir, 'media', 'raw', 'images', 'we'),
+    os.path.join(home_dir, 'media', 'raw', 'rss'),
+    os.path.join(home_dir, 'media', 'raw', 'bd'),
+    os.path.join(home_dir, 'media', 'raw', 'texts'),
 ]
 
 class Basic():
@@ -62,7 +63,7 @@ class Basic():
     #     catalog_date = os.path.getmtime(path):
         
 # Создаём внутри корневого каталога сайта рабочие папки
-    def check_dirs(paths):
+    def check_dirs(self, paths):
         for path in paths:
             if not os.path.exists(path):
                 os.mkdir(path)
@@ -83,15 +84,16 @@ class Info():
             objs = json.load(f)
     
     team = [
-        'Агент',
-        'Юрист'
+        'Agent',
+        'Law'
     ]
 
     dist_rss = os.path.abspath(
             search(
                 os.path.join(
                     home_dir,
-                    'static',
+                    'media',
+                    'raw',
                     'rss'
                     )
                 )
@@ -100,7 +102,8 @@ class Info():
                 search(
                     os.path.join(
                         home_dir,
-                        'static',
+                        'media',
+                        'raw',
                         'texts'
                         )
                     )
@@ -109,7 +112,8 @@ class Info():
             search(
                 os.path.join(
                     home_dir,
-                    'static',
+                    'media',
+                    'raw',
                     'bd'
                     )
                 )
@@ -118,9 +122,10 @@ class Info():
                 search(
                     os.path.join(
                         home_dir,
-                        'static',
+                        'media',
+                        'raw',
                         'images',
-                        'Команда'
+                        'we'
                         )
                     )
                 )
@@ -135,56 +140,76 @@ class Info():
             )
 
         shutil.copy(rss_path, self.dist_rss)
-
+        os.rename(
+                os.path.join(self.dist_rss, 'Список RSS каналов.txt'),
+                os.path.join(self.dist_rss, 'rss_list.txt')
+            )
     def text(self):
-        for obj in self.objs:
+        number_id = 0
+
+        for obj in sorted(self.objs):
+            number_id += 1
+            
             text_path = search(
                 os.path.join(
                     'Данные для работы',
                     'Авито',
-                    '{0}.txt'.format(self.objs[obj]['Адрес'])
+                    f'{self.objs[obj]["Адрес"]}.txt'
                     )
                 )
 
             shutil.copy(text_path, self.dist_texts)
+            os.rename(
+                search(os.path.join(self.dist_texts,f'{self.objs[obj]["Адрес"]}.txt')),
+                os.path.join(self.dist_texts, f'{number_id}.txt')
+            )
 # Функция копирования файлов БД в каталог dist_path_bd
     def bd(self):
-        catalog = os.path.join(
-            'Данные для работы',
-            'Социалочка',
-            'Каталог.json'
-            )
-        bd_we = search(
-            os.path.join(
-                'Данные для работы',
-                'Социалочка',
-                'Наши ресурсы.json'
-                )
-            )
-        services = search(
-            os.path.join(
-                'Данные для работы',
-                'Социалочка',
-                'Наши услуги.json'
-                )
-            )
+        number_id = 0
 
+        catalog = {
+            'origin': os.path.join('Данные для работы', 'Социалочка', 'Каталог.json'),
+            'name': 'Каталог.json',
+            'rename': 'catalog.json',
+        }
+        bd_we = {
+            'origin': search(os.path.join('Данные для работы', 'Социалочка', 'Наши ресурсы.json')),
+            'name': 'Наши ресурсы.json',
+            'rename': 'we.json',
+        }
+        services = {
+            'origin': search(os.path.join('Данные для работы', 'Социалочка', 'Наши услуги.json')),
+            'name': 'Наши услуги.json',
+            'rename': 'services.json',
+        }
         for bd in catalog, bd_we, services:
-            shutil.copy(bd, self.dist_bd)
+            shutil.copy(bd['origin'], self.dist_bd)
+            os.rename(
+                search(os.path.join(self.dist_bd, bd['name'])),
+                os.path.join(self.dist_bd, bd['rename'])
+            )
 
-        for obj in self.objs:
+        for obj in sorted(self.objs):
+            number_id += 1
+
             bd_path = search(
                 os.path.join(
                     'Данные для работы',
                     'БД',
-                    '{}.json'.format(self.objs[obj]['Адрес'])
+                    f'{self.objs[obj]["Адрес"]}.json'
                     )
                 )
 
             # if not os.path.exists(dist_path_bd):
             shutil.copy(bd_path, self.dist_bd)
+            os.rename(
+                search(os.path.join(self.dist_bd, f'{self.objs[obj]["Адрес"]}.json')),
+                os.path.join(self.dist_bd, f'{number_id}.json')
+                )
 # Функция копирования Фото в каталог dist_path_photo
     def photo(self):
+        number_id = 0
+
         for member in self.team:
             member_face_path = search(
                 os.path.join(
@@ -198,14 +223,17 @@ class Info():
             # if not os.path.exists(dist_member_path):
             shutil.copy(member_face_path, self.dist_photo_member)
 
-        for obj in self.objs:
-            Basic.check_dirs([
+        for obj in sorted(self.objs):
+            number_id += 1
+
+            Basic().check_dirs([
                 os.path.join(
                     home_dir,
-                    'static',
+                    'media',
+                    'raw',
                     'images',
-                    'Объекты',
-                    obj
+                    'properties',
+                    f'{number_id}'
                     )
                 ])
 
@@ -227,17 +255,19 @@ class Info():
                 search(
                     os.path.join(
                         home_dir,
-                        'static',
+                        'media',
+                        'raw',
                         'images',
-                        'Объекты',
-                        obj
+                        'properties',
+                        f'{number_id}'
                         )
                     )
                 )
 
             # if not os.path.exists(photo_face_path):
             shutil.copy(photo_face_path, dist_path_photo)
-            
+            os.rename(os.path.join(dist_path_photo, 'Морда.jpeg'), os.path.join(dist_path_photo, 'face.jpeg'))
+
             for dirs in pathlib.Path(photo_path).iterdir():
                 if dirs.is_dir():
                     files = [file for file in pathlib.Path(dirs).iterdir() if file.stat().st_size > 0 and file.exists()]
@@ -260,27 +290,28 @@ class Info():
 
         # ftp = FTP(ftp_host, ftp_user, ftp_password)
         # ftp.cwd('rossoshrealty')
+        # print(ftp.getwelcome())
 
-        for dir in base:
-            if dir.is_dir():
-                for smth in pathlib.Path(dir).iterdir():
-                    if smth.is_dir():
-                        for smth_2 in pathlib.Path(smth).iterdir():
-                            print(smth_2)
-                            if smth_2.is_dir():
-                                for smth_3 in pathlib.Path(smth_2).iterdir():
-                                    print(smth_3)
-                                    if smth_3.is_dir():
-                                        for smth_4 in pathlib.Path(smth_3).iterdir():
-                                            print(smth_4)
-                    else:
-                        print(smth)
+        # for dir in base:
+        #     if dir.is_dir():
+        #         for smth in pathlib.Path(dir).iterdir():
+        #             if smth.is_dir():
+        #                 for smth_2 in pathlib.Path(smth).iterdir():
+        #                     print(smth_2)
+        #                     if smth_2.is_dir():
+        #                         for smth_3 in pathlib.Path(smth_2).iterdir():
+        #                             print(smth_3)
+        #                             if smth_3.is_dir():
+        #                                 for smth_4 in pathlib.Path(smth_3).iterdir():
+        #                                     print()
+        #             else:
+        #                 print(smth)
 
 
 
-# Basic.check_dirs(list_of_dirs)
-# Info().text()
-# Info().rss()
-# Info().bd()
-# Info().photo()
-Info().ftp()
+Basic().check_dirs(list_of_dirs)
+Info().text()
+Info().rss()
+Info().bd()
+Info().photo()
+# Info().ftp()
